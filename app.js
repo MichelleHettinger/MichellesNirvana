6033f384262698ea265b4228e20e605cf4628ee3
Michelle Hettinger <Michelle Hettinger>
2016-11-22 19:38:07 -0500

    more progress towards aws deployment

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
@@ -17,20 +21,24 @@ app.set('view engine', 'handlebars');
 
 //Serve static content for the app from the "public" directory in the application directory.
 //Public directory becomes /static in html files
-app.use('/static', express.static(__dirname + '/public'));
+//app.use('/static', express.static(__dirname + '/public'));
 
 //app.use(express.static(__dirname + '/public'));
 //Looks in public for static file to server
 
+
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.text());
+app.use(cookieParser());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 
+
+
+
 //ROUTES HERE
-
-
 //Root (www.michellesnirvana.com)
 app.get('/', function(req,res) {
 	res.render('index');
@@ -64,3 +72,28 @@ app.get('/writing', function(req,res) {
 app.listen(PORT, function(){
 	console.log('App listening on PORT ' + PORT);
 })
+
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
